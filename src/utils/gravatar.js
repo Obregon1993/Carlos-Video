eeec8182ea87d7ab071a1d49ea978731636729bc
Carlos Javier <cjangie13@gmail.com>
2020-11-30 18:27:33 -0500

    adding gravatar to teh header

--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,7 +2,9 @@ import md5 from 'md5';
 
 const gravatar = (email) => {
   const base = 'http://gravatar.com/avatar/';
-  const formatteEmail = (email).trim().toLowercase();
-  const hash = md5(formatteEmail, { encoding: 'binary' });
+  const formattedEmail = (email).trim().toLowercase();
+  const hash = md5(formattedEmail, { encoding: 'binary' });
   return `${base}${hash}`;
 };
+
+export default gravatar;
