8c7872cb829e7c37d520a3344bf2e66ceb3010bd
Carlos Javier <cjangie13@gmail.com>
2020-12-01 16:46:06 -0500

    login and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -9,6 +10,9 @@ import userIcon from '../assets/static/user-icon.png';
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+  const handleLogOut = () => {
+    props.logoutRequest({});
+  };
   return (
     <header className='header'>
 
@@ -24,8 +28,15 @@ const Header = (props) => {
           <p>Profile</p>
         </div>
         <ul>
-          <li><a href='/'>Account</a></li>
-          <li><Link to='/login'>Log in</Link></li>
+          { hasUser ?
+            <li><a href='/'>{user.name}</a></li> :
+            null}
+          {
+            hasUser ? <li><a href='#logout' onClick={handleLogOut}>Log out</a></li> :
+            // eslint-disable-next-line indent
+            <li><Link to='/login'>Log in</Link></li>
+          }
+
         </ul>
       </div>
     </header>
@@ -37,4 +48,8 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
